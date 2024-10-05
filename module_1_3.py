58133a9e2701c52efd861294488fd836b013d19d
Helen2006-2024 <elenabelikova298@gmail.com>
2024-10-05 13:11:07 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,20 @@
+name = 'Name' + ': ' + 'Helen'
+print(name)
+age = 44
+print(age)
+new_age = 45
+print(new_age)
+print('Is' , 'Student', ':')
+is_student = 'Helen'
+print('Helen' in is_student)
+
+
+name = 'Name' + ': ' + 'Helen'
+print(name)
+print('Age' , ':' , 44)
+print('New'+ 'Age'+':', 45)
+print('Is_Student' + ':')
+is_student = 'Helen'
+print('Helen' in is_student)
+
+
